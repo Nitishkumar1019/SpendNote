e026d841dd167d2686c6e7d3be801a677a6be282
Nitish kumar <nitishkumar735430@gmail.com>
2026-09-19 18:16:29 +0530

    Improved add expense security

--- add_expense.php
+++ add_expense.php
@@ -6,17 +6,27 @@ $description=$_POST['description'];
 $category=$_POST['category'];
 $expense_date=$_POST['expense_date'];
 
-$sql="INSERT INTO expenses (amount,description,category,expense_date) VALUES ('$amount','$description','$category','$expense_date') ";
+// $sql="INSERT INTO expenses (amount,description,category,expense_date) VALUES ('$amount','$description','$category','$expense_date') ";
+$sql="INSERT INTO expenses (amount,description,category,expense_date) VALUES (?,?,?,?) ";
 
-$result=mysqli_query($con,$sql);
+$stmt=mysqli_prepare($con,$sql);
 
-header ("Location:index.php")
+
+mysqli_stmt_bind_param($stmt,"dsss",$amount,$description,$category,$expense_date);
+
+
+
+// $result=mysqli_query($con,$sql);
+$result=mysqli_stmt_execute($stmt);
 
 if($result){
     // echo "Expense added";
+    header ("Locatin:index.php");
+    exit;
 }
 else{
-    echo "error".mysqli_error($con);
+    // echo "error".mysqli_error($con);
+    echo "error".mysqli_stmt__error($stmt);
 }
 // echo "amount:".$amount. "<br>";
 // echo "description:".$description. "<br>";
