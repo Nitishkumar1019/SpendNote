1ef9eb523244253077657fe895a0ba3110316fbe
Nitish kumar <nitishkumar735430@gmail.com>
2026-09-19 23:34:23 +0530

    Added update expense validation

--- update_expense.php
+++ update_expense.php
@@ -1,11 +1,49 @@
 <?php
 include ("db.php");
 
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    if(!isset($_POST['id'])||
+        !isset($_POST['amount'])||
+        !isset($_POST['description'])||
+        !isset($_POST['category'])||
+        !isset($_POST['expense_date'])
+        ){
+            die("Please submit the expense form");
+        }
+}
 $id=$_POST['id'];
+if($id<=0){
+    die ("Invalid id");
+}
+
 $amount=$_POST['amount'];
+if($amount<=0){
+    die("Amount must be greater than 0");
+}
+
 $description=$_POST['description'];
+if(empty(trim($description))){
+    die ("Description should not be empty");
+}
+
 $category=$_POST['category'];
+
+$allowed_category=['Grocery','Travel','Food','Cloth','Study Material','Other'];
+
+if(!in_array($category,$allowed_category)){
+    die ("Invalid category");
+}
+
 $expense_date=$_POST['expense_date'];
+if(empty($expense_date)){
+    die("Date cannot be empty");
+}
+
+$date=DateTime::createFromFormat('Y-m-d',$expense_date);
+if(!$date || $date->format('Y-m-d')!==$expense_date){
+    die("Invalid date");
+}
+
 
 // $sql="UPDATE expenses SET amount='$amount', description='$description', category='$category', expense_date='$expense_date'  WHERE id=$id";
 
@@ -23,6 +61,6 @@ if($result){
     exit;
 }
 else{
-    echo "Error:".mysqli_error($con);
+    echo "Error:".mysqli_stmt_error($stmt);
 }
 ?>
